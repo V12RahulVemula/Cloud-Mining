8c516b1fdb006d5dfb7b567e2fd342e8a782194b
Rahul Vemula <55878821+vssrrahul@users.noreply.github.com>
2021-11-27 14:03:54 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+from hashlib import sha256
+MAX_NONCE=100000000
+def SHA256(text):
+  return sha256(text.encode("ascii")).hexdigest()
+def mine(block_number,transaction,previous_hash,prefix_zeros):
+  prefix_str='0'*prefix_zeros
+  for nonce in range(MAX_NONCE):
+    text= str(block_number) + transaction + previous_hash + str(nonce)
+    hash = SHA256(text)
+    # print(hash)
+    if hash.startswith(prefix_str):
+      print("Bitcoin mined with nonce value :",nonce)
+      return hash
+  print("Could not find a hash in the given range of upto", MAX_NONCE)
+transactions='''
+A->B->10
+B->c->5
+'''
+difficulty = 2
+
+import time as t
+begin=t.time()
+new_hash = mine(684260,transactions,"000000000000000000006bd3d6ef94d8a01de84e171d3553534783b128f06aad",difficulty)
+print("Hash value : ",new_hash)
+time_taken=t.time()- begin
+print("The mining process took ",time_taken,"seconds")
